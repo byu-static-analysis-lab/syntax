ae1d9ea54c74b4ece6cae863e6e273542bdc471b
Tim Whiting <tim@whitings.org>
2023-06-01 16:03:35 -0600

    user lambdas

--- lib/src/scheme/helpers.dart
+++ lib/src/scheme/helpers.dart
@@ -6,7 +6,7 @@ extension CallLam on Exp {
     final topLam = -1;
     var (lams, labels) = callLam(enclosingLam: topLam);
     while (lams.isNotEmpty) {
-      final lam = Output<ILambda>();
+      final lam = Output<ULambda>();
       lams = lams.removeAt(0, lam);
       final (newLambdas, newLabels) =
           lam.value!.body.callLam(enclosingLam: (lam.value as Exp).label);
@@ -16,9 +16,10 @@ extension CallLam on Exp {
     return labels;
   }
 
-  (IList<ILambda>, IMap<int, int>) callLam({int enclosingLam = -1}) =>
+  (IList<ULambda>, IMap<int, int>) callLam({int enclosingLam = -1}) =>
       switch (this) {
-        ILambda() => ([this as ILambda].lock, IMap<int, int>()),
+        ULambda() => ([this as ULambda].lock, IMap<int, int>()),
+        KLambda(:final body) => body.callLam(enclosingLam: enclosingLam),
         App(:final fun, :final args) => args
             .fold(
                 fun.callLam(enclosingLam: enclosingLam),
@@ -39,18 +40,18 @@ extension CallLam on Exp {
       };
 }
 
-extension AddAllX on (IList<ILambda>, IMap<int, int>) {
-  (IList<ILambda>, IMap<int, int>) addAll(
-          (IList<ILambda>, IMap<int, int>) other) =>
+extension AddAllX on (IList<ULambda>, IMap<int, int>) {
+  (IList<ULambda>, IMap<int, int>) addAll(
+          (IList<ULambda>, IMap<int, int>) other) =>
       (this.$1.addAll(other.$1), this.$2.addAll(other.$2));
 
-  (IList<ILambda>, IMap<int, int>) addCall(int label, int enclosingLam) =>
+  (IList<ULambda>, IMap<int, int>) addCall(int label, int enclosingLam) =>
       (this.$1, this.$2.add(label, enclosingLam));
-  static final empty = (IList<ILambda>(), IMap<int, int>());
+  static final empty = (IList<ULambda>(), IMap<int, int>());
 }
 
 extension CallLamBody on Body {
-  (IList<ILambda>, IMap<int, int>) callLam({int enclosingLam = -1}) =>
-      exps.fold((IList<ILambda>(), IMap<int, int>()),
+  (IList<ULambda>, IMap<int, int>) callLam({int enclosingLam = -1}) =>
+      exps.fold((IList<ULambda>(), IMap<int, int>()),
           (acc, exp) => acc.addAll(exp.callLam(enclosingLam: enclosingLam)));
 }
