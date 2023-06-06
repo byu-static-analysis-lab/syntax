c262ab4b1396a7cc26aba39abf93389b6585c67d
Tim Whiting <tim@whitings.org>
2023-06-06 13:22:29 -0600

    also cache which variables are in which lambdas

--- lib/src/scheme/helpers.dart
+++ lib/src/scheme/helpers.dart
@@ -2,35 +2,35 @@ import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:syntax/syntax.dart';
 
 extension CallLam on Exp {
-  IMap<int, int> get callLams {
+  IMap<int, int> get exprLams {
     final topLam = -1;
-    var (lams, labels) = callLam(enclosingLam: topLam);
+    var (lams, labels) = exprLam(enclosingLam: topLam);
     while (lams.isNotEmpty) {
       final lam = Output<ULambda>();
       lams = lams.removeAt(0, lam);
-      final (newLambdas, newLabels) = lam.value!.body.callLam(enclosingLam: (lam.value as Exp).label);
+      final (newLambdas, newLabels) = lam.value!.body.exprLam(enclosingLam: (lam.value as Exp).label);
       lams = lams.addAll(newLambdas);
       labels = labels.addAll(newLabels);
     }
     return labels;
   }
 
-  (IList<ULambda>, IMap<int, int>) callLam({int enclosingLam = -1}) => switch (this) {
+  (IList<ULambda>, IMap<int, int>) exprLam({int enclosingLam = -1}) => switch (this) {
         ULambda() => ([this as ULambda].lock, IMap<int, int>()),
-        KLambda(:final body) => body.callLam(enclosingLam: enclosingLam),
+        KLambda(:final body) => body.exprLam(enclosingLam: enclosingLam),
         App(:final fun, :final args) => args
-            .fold(fun.callLam(enclosingLam: enclosingLam), (acc, arg) => acc.addAll(arg.callLam(enclosingLam: enclosingLam)))
-            .addCall(label, enclosingLam),
-        Ref() => AddAllX.empty,
+            .fold(fun.exprLam(enclosingLam: enclosingLam), (acc, arg) => acc.addAll(arg.exprLam(enclosingLam: enclosingLam)))
+            .addExpr(label, enclosingLam),
+        Ref() => AddAllX.empty.addExpr(label, enclosingLam),
         ILit() => AddAllX.empty,
         EVoid() => AddAllX.empty,
         Undefined() => AddAllX.empty,
-        Begin(:final body) => body.callLam(enclosingLam: enclosingLam),
-        SetExp(:final value) => value.callLam(enclosingLam: enclosingLam),
+        Begin(:final body) => body.exprLam(enclosingLam: enclosingLam),
+        SetExp(:final value) => value.exprLam(enclosingLam: enclosingLam),
         IfExp(:final condition, :final ifTrue, :final ifFalse) => condition
-            .callLam(enclosingLam: enclosingLam)
-            .addAll(ifTrue.callLam(enclosingLam: enclosingLam))
-            .addAll(ifFalse.callLam(enclosingLam: enclosingLam)),
+            .exprLam(enclosingLam: enclosingLam)
+            .addAll(ifTrue.exprLam(enclosingLam: enclosingLam))
+            .addAll(ifFalse.exprLam(enclosingLam: enclosingLam)),
         _ => throw UnimplementedError('$this')
       };
 }
@@ -38,11 +38,11 @@ extension CallLam on Exp {
 extension AddAllX on (IList<ULambda>, IMap<int, int>) {
   (IList<ULambda>, IMap<int, int>) addAll((IList<ULambda>, IMap<int, int>) other) => (this.$1.addAll(other.$1), this.$2.addAll(other.$2));
 
-  (IList<ULambda>, IMap<int, int>) addCall(int label, int enclosingLam) => (this.$1, this.$2.add(label, enclosingLam));
+  (IList<ULambda>, IMap<int, int>) addExpr(int label, int enclosingLam) => (this.$1, this.$2.add(label, enclosingLam));
   static final empty = (IList<ULambda>(), IMap<int, int>());
 }
 
 extension CallLamBody on Body {
-  (IList<ULambda>, IMap<int, int>) callLam({int enclosingLam = -1}) =>
-      exps.fold((IList<ULambda>(), IMap<int, int>()), (acc, exp) => acc.addAll(exp.callLam(enclosingLam: enclosingLam)));
+  (IList<ULambda>, IMap<int, int>) exprLam({int enclosingLam = -1}) =>
+      exps.fold((IList<ULambda>(), IMap<int, int>()), (acc, exp) => acc.addAll(exp.exprLam(enclosingLam: enclosingLam)));
 }
